d0f8c4b18dd54bf0c7cd757b08cda316904273c2
Rachit garg <rachitgarg2404@gmail.com>
2025-11-03 05:25:28 +0000

    Before: Can you now add a proper database inside this project. That will take all the details of mentors, st...

--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -8,11 +8,6 @@
  * @module
  */
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
 import type * as alumni from "../alumni.js";
 import type * as auth_emailOtp from "../auth/emailOtp.js";
 import type * as auth from "../auth.js";
@@ -21,6 +16,12 @@ import type * as files from "../files.js";
 import type * as http from "../http.js";
 import type * as testData from "../testData.js";
 import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -40,11 +41,15 @@ declare const fullApi: ApiFromModules<{
   testData: typeof testData;
   users: typeof users;
 }>;
+declare const fullApiWithMounts: typeof fullApi;
+
 export declare const api: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "public">
 >;
 export declare const internal: FilterApi<
-  typeof fullApi,
+  typeof fullApiWithMounts,
   FunctionReference<any, "internal">
 >;
+
+export declare const components: {};
